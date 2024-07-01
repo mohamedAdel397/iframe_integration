121a1c6a1ab98b8f80332415ad26e7b0b1ec705b
mohamedAdel397 <105241891+mohamedAdel397@users.noreply.github.com>
2024-07-01 13:02:14 +0300

    Update ifarme-again.js
    l

--- ifarme-again.js
+++ ifarme-again.js
@@ -2,7 +2,7 @@
 const container = document.getElementById("payment-container");
 if (container) {
     const iframe = document.createElement("iframe");
-    iframe.src = "https://mohamedadel397.github.io/iframe_integration/iframe.html"; // The URL of the iframe content
+    iframe.src = "https://mohamedadel397.github.io/iframe_integration/ifarme-again.html"; // The URL of the iframe content
     iframe.width = container.getAttribute('data-iframe-width') || "100%";
     iframe.height = container.getAttribute('data-iframe-height') || "400px";
     iframe.style.border = "none";
