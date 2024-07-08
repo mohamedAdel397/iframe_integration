76ab6331dbeb0740526688530c22edefd6b0b0e9
mohamedAdel397 <105241891+mohamedAdel397@users.noreply.github.com>
2024-07-08 11:42:46 +0300

    Update iframe.js
    vffdm

--- iframe.js
+++ iframe.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const container = document.getElementsByClass("payment-container");
+    const container = document.getElementsByClassName("payment-container");
     if (container) {
         const iframe = document.createElement("iframe");
         iframe.src = "https://mohamedadel397.github.io/iframe_integration/iframe.html"; // The URL of the iframe content
